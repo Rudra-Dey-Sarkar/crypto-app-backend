59a98fc9cf33cd99164a3acc989c2107e2c89ef1
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-01-14 21:10:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const ConnectDB = require("./src/config/db");
+const express = require("express");
+const cors = require("cors");
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+const app = express()
+app.use(cors(corsOptions));
+app.use(express.json());
+
+ConnectDB();
+
+app.get("/", async (req, res)=>{
+
+    res.json("working");
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("App is listening in the port :-", process.env.PORT);
+})
---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const ConnectDB = ()=>{
+mongoose.connect(process.env.DB).then(()=>{
+    console.log("Database connected");
+}).catch((errors)=>{
+
+    console.log("Cannot connect to the database due to :-", errors);
+})
+}
+
+module.exports = ConnectDB;
